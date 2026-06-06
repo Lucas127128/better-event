b7e563670e3dc8ddf1eb5ba003728fa0f5f4b58a
Lucas Leung <kayauleung2024@gmail.com>
2026-06-06 13:19:37 +0800

    Support disabling an event

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 type EventHandler<T> = (data: T) => Promise<void>;
 type EventMap = Record<string, { handler: EventHandler<any>; signal?: AbortSignal }>;
 
-/**
- * Creates an event emitter that allows you to define and emit events with async handlers.
- * @param params - The event emitter configuration.
- * @returns An object with an `emit` method to trigger events.
- */
-export function createEventEmitter<T extends EventMap>(params: {
-  on: T;
-  debug?: { name: string };
-}) {
-  const events = params.on;
-  const debug = params.debug ?? false;
+function attachAbortListener(events: EventMap, debug: boolean) {
   for (const [eventKey, event] of Object.entries(events)) {
     if (event.signal) {
       event.signal.addEventListener('abort', () => {
@@ -22,6 +12,20 @@ export function createEventEmitter<T extends EventMap>(params: {
       });
     }
   }
+}
+
+/**
+ * Creates an event emitter that allows you to define and emit events with async handlers.
+ * @param params - The event emitter configuration.
+ * @returns An object with an `emit` method to trigger events.
+ */
+export function createEventEmitter<T extends EventMap>(params: {
+  on: T;
+  debug?: { name: string };
+}) {
+  const events = params.on;
+  const debug = params.debug ?? undefined;
+  attachAbortListener(events, !!debug);
 
   return {
     emit: async <K extends keyof T>(eventKey: K, data: Parameters<T[K]['handler']>[0]) => {
@@ -34,6 +38,12 @@ export function createEventEmitter<T extends EventMap>(params: {
           eventKey: String(eventKey),
           data,
         });
+      }
+    },
+    disable: (eventKey: keyof T) => {
+      events[eventKey].handler = () => Promise.resolve();
+      if (debug) {
+        console.log(`Event ${String(eventKey)} disabled`);
       }
     },
   };
