6e26c8985aaf047ee6fb7c6b6714454b44ca5a42
Lucas Leung <kayauleung2024@gmail.com>
2026-06-08 21:23:11 +0800

    Add more lint rules

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'oxlint';
 
 export default defineConfig({
-  plugins: ['typescript', 'unicorn', 'oxc'],
-  rules: { 'no-var': 'error', 'typescript/await-thenable': 'error' },
+  plugins: ['typescript', 'unicorn', 'oxc', 'import'],
+  rules: { 'no-var': 'error', 'typescript/await-thenable': 'error', 'import/no-cycle': 'error' },
   env: {
     builtin: true,
   },
